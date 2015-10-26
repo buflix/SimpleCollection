eca6992cce405e7ca4a841b6fde3538b2949af5a
Felix Buchheim <felix.buchheim@unister.de>
2015-10-26 08:36:38 +0100

    rename to SimpleCollection and implement Interface

---
+++ src/SimpleCollection/AbstractEntity.php
@@ -0,0 +1,33 @@
+<?php
+namespace SimpleCollection;
+
+/**
+ * abstract entity
+ *
+ * @copyright Felix Buchheim
+ * @author    Felix Buchheim <hanibal4nothing@gmail.com>
+ */
+abstract class AbstractEntity implements EntityInterface
+{
+    /**
+     * return this entity as array
+     *
+     * @see EntityInterface::toArray
+     * @return array
+     */
+    public function toArray()
+    {
+        $aReturn = array();
+
+        foreach (get_object_vars($this) as $sKey => $mValue) {
+            if ($mValue instanceof EntityInterface or true === method_exists($mValue, 'toArray')) {
+                $aReturn[$sKey] = $mValue->toArray();
+            }
+            else {
+                $aReturn[$sKey] = $mValue;
+            }
+        }
+
+        return $aReturn;
+    }
+}
